98f550c582e5e3d0fbaea9ae7dc97866e524907f
Peter Ableda <scotty@secretsaucepartners.com>
2015-03-16 16:15:27 +0100

    Create placeholder for convert command

--- lib/csv2avro.rb
+++ lib/csv2avro.rb
@@ -1,4 +1,7 @@
 require "csv2avro/version"
 
 class CSV2Avro
+  def convert(source_path, target_path, options)
+    p "Convert file from #{source_path} to #{target_path} with options #{options}"
+  end
 end
