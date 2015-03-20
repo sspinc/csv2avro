185bcb7060a0ec6d6165ffe8cee4e323893e2285
Peter Ableda <scotty@secretsaucepartners.com>
2015-03-20 11:45:01 +0100

    Add test to check working with tsv data and DRY spec

--- spec/csv2avro/converter_spec.rb
+++ spec/csv2avro/converter_spec.rb
@@ -2,41 +2,69 @@ require 'spec_helper'
 
 RSpec.describe CSV2Avro::Converter do
   describe '#perform' do
-    context 'csv data with string and integer columns' do
-      let(:input_string) do
-        csv_string = CSV.generate do |csv|
-          csv << %w[id name description]
-          csv << %w[1 dresses Dresses]
-          csv << %w[2 female-tops]
+    context 'schema with string and integer columns' do
+      let(:schema) do
+        StringIO.new(
+          {
+            type: 'record',
+            name: 'categories',
+            fields: [
+              { name: 'id', type: 'int' },
+              { name: 'name', type: 'string' },
+              { name: 'description', type: ['string','null'] }
+            ]
+          }.to_json
+        )
+      end
+
+      context 'separated with commas (csv)' do
+        let(:input) do
+          StringIO.new(
+            csv_string = CSV.generate do |csv|
+              csv << %w[id name description]
+              csv << %w[1 dresses Dresses]
+              csv << %w[2 female-tops]
+            end
+            )
+        end
+
+        subject(:avro_io) do
+          CSV2Avro::Converter.new(input, schema, StringIO.new, {}).perform
+        end
+
+        it 'should store the data with the given schema' do
+          expect(CSV2Avro::Reader.new(avro_io).perform).to eq(
+            [
+              { "id"=>1, "name"=>"dresses",     "description"=>"Dresses" },
+              { "id"=>2, "name"=>"female-tops", "description"=>nil }
+            ]
+          )
         end
       end
 
-      let(:schema_string) do
-        {
-          type: 'record',
-          name: 'categories',
-          fields: [
-            { name: 'id', type: 'int' },
-            { name: 'name', type: 'string' },
-            { name: 'description', type: ['string','null'] }
-          ]
-        }.to_json
-      end
+      context 'separated with tabs (tsv)' do
+        let(:input) do
+          StringIO.new(
+            csv_string = CSV.generate({col_sep: "\t"}) do |csv|
+              csv << %w[id name description]
+              csv << %w[1 dresses Dresses]
+              csv << %w[2 female-tops]
+            end
+          )
+        end
 
-      subject(:avro_io) do
-        input = StringIO.new(input_string)
-        schema = StringIO.new(schema_string)
+        subject(:avro_io) do
+          CSV2Avro::Converter.new(input, schema, StringIO.new, {delimiter: "\t"}).perform
+        end
 
-        CSV2Avro::Converter.new(input, schema, StringIO.new, {}).perform
-      end
-
-      it 'should work' do
-        expect(CSV2Avro::Reader.new(avro_io).perform).to eq(
-          [
-            { "id"=>1, "name"=>"dresses", "description"=>"Dresses" },
-            { "id"=>2, "name"=>"female-tops", "description"=>nil }
-          ]
-        )
+        it 'should store the data with the given schema' do
+          expect(CSV2Avro::Reader.new(avro_io).perform).to eq(
+            [
+              { "id"=>1, "name"=>"dresses",     "description"=>"Dresses" },
+              { "id"=>2, "name"=>"female-tops", "description"=>nil }
+            ]
+          )
+        end
       end
     end
   end
