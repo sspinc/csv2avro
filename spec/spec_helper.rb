d3eba279b3de0fbce786c5b7323c78813eb09346
Peter Ableda <scotty@secretsaucepartners.com>
2015-04-30 15:43:17 +0200

    Remove temporary files after tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,3 +5,11 @@ require 'csv2avro/avro_writer'
 require 'json'
 
 Dir[File.dirname(__FILE__) + '/support/**/*.rb'].each {|f| require f }
+
+RSpec.configure do |config|
+  config.after(:all) do
+    Dir["./spec/support/*.avro", "./spec/support/*.bad*"].each do |file|
+      File.delete(file)
+    end
+  end
+end
