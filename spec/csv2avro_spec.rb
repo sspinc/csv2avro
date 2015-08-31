66c9d9a5d143c9aa4617fadcfb2c1874e4f748eb
Peter Marton <martonpe@secretsaucepartners.com>
2015-08-31 17:27:41 +0200

    Add failing tests

--- spec/csv2avro_spec.rb
+++ spec/csv2avro_spec.rb
@@ -31,5 +31,31 @@ RSpec.describe CSV2Avro do
         )
       end
     end
+
+    ARGV.replace ['./spec/support/data_parenthesized.csv']
+    subject(:converter) { CSV2Avro.new(options) }
+
+    it 'should write errors to STDERR if parenthesized' do
+      expect { converter.convert }.to output("line 4: Missing value at name\nline 7: Unable to parse\n").to_stderr
+    end
+
+    it 'should have a bad row if parenthesized' do
+      File.open('./spec/support/data.bad.csv', 'r') do |file|
+        expect(file.read).to eq("id,name,description\n3,,Bras\n")
+      end
+    end
+
+    it 'should contain the avro data if parenthesized' do
+      File.open('./spec/support/data.avro', 'r') do |file|
+        expect(AvroReader.new(file).read).to eq(
+          [
+            { 'id'=>1, 'name'=>'dresses',     'description'=>'Dresses' },
+            { 'id'=>2, 'name'=>'female-tops', 'description'=>nil },
+            { 'id'=>4, 'name'=>'male-tops',   'description'=>"Male Tops\nand Male Shirts"},
+            { 'id'=>6, 'name'=>'male-shoes', 'description'=>'Male Shoes'}
+          ]
+        )
+      end
+    end
   end
 end
